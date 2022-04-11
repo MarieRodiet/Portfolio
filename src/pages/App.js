b20235fdee029a0070a555b1accb5ec771a55a53
Marie Rodiet <marie.rodiet@gmail.com>
2022-04-11 09:33:05 +0200

    First commit

---
+++ src/pages/App.js
@@ -0,0 +1,13 @@
+import './../style/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Marie Moore's Portfolio</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
