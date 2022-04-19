26bab1f8239195e5c6768a971801ff2d18a35961
Marie Rodiet <marie.rodiet@gmail.com>
2022-04-19 17:12:11 +0200

    Add navigation component

---
+++ src/components/Navigation.js
@@ -0,0 +1,10 @@
+function Navigation() {
+    return (
+        <nav><ul>
+            <li>Projets</li>
+            <li>CV</li>
+            <li>Contact</li></ul></nav>
+    )
+}
+
+export default Navigation;
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,9 +1,11 @@
 import './../style/App.css';
+import Navigation from "./../components/Navigation.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navigation />
         <h1>Marie Moore's Portfolio</h1>
       </header>
     </div>
