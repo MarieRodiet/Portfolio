f583458ae8435ebc04ec7fc2b10950457f96e031
Marie Rodiet <marie.rodiet@gmail.com>
2022-04-23 09:20:57 +0200

    Fix navigation layout

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,13 +1,18 @@
 import './../style/App.css';
 import Navigation from "./../components/Navigation.js";
+import styled from 'styled-components';
+
+const Header = styled.div`
+background-color: #3E1348;
+color: white`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header>
         <Navigation />
         <h1>Marie Moore's Portfolio</h1>
-      </header>
+      </Header>
     </div>
   );
 }
