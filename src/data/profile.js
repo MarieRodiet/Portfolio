e7e663fc1b9b3b469d01b1c448ba22ae1cff213d
Marie Moore <68657110+MarieRodiet@users.noreply.github.com>
2024-01-25 11:29:35 +0100

    update des cv et intro

--- src/data/profile.js
+++ src/data/profile.js
@@ -1,16 +1,16 @@
 export const profile = {
   name: 'Marie Moore',
-  email: 'mailto:marie.rodiet@gmail.com',
+  email: 'mailto:marierob.moore@gmail.com',
   linkedIn: 'https://www.linkedin.com/in/marie-rodiet-moore/',
   github: 'https://github.com/MarieRodiet',
   anArray: [
-    "I'm a full-stack web developer based in Paris. Currently, I'm gaining hands-on experience and enhancing my Java and Angular skills through a degree internship. I initiated my programming journey in 2020 with C# in the USA, then shifted towards JavaScript and React, earning a Bachelor's degree in Frontend Web Development.",
-    "Now focused on backend development with Java EE, Spring Boot, and SQL, I'm actively contributing to a Parisian company. Feel free to connect through the form below or your preferred social network.",
+    "I am a full-stack web developer based in Paris, specializing in front-end development. I am currently completing my apprenticeship in a company where I have learned a lot. I started my journey into programming in 2020 in the United States, and then shifted towards specializing in Front-end development. This led me to obtain a Bachelor's degree (RNCP Level 6) in web development with Javascript React.",
+    'I continue to actively contribute within my Parisian company while exploring where the adventure will take me after the end of my contract. Feel free to contact me via the form below or your preferred social network.',
     'Thanks for visiting!',
   ],
   frArray: [
-    "Je suis un développeur web full-stack basé à Paris. Actuellement, j'acquiers une expérience pratique et renforce mes compétences en Java et Angular grâce à un stage diplômant en entreprise. J'ai amorcé mon parcours de programmation en 2020 avec C# aux États-Unis, pour ensuite me tourner vers JavaScript et React, obtenant ainsi une licence (RNCP Niveau 6) en développement web frontend.",
-    "Aujourd'hui concentrée sur le développement backend avec Java EE, Spring Boot et SQL, je contribue activement au sein d'une entreprise parisienne. N'hésitez pas à me contacter via le formulaire ci-dessous ou votre réseau social préféré.",
+    "Je suis une développeuse web full-stack basée à Paris et spécialisée dans le front. Je suis actuellement en fin de parcours d'alternance au sein d'une entreprise dans laquelle j'ai beaucoup appris. J'ai amorcé mon aventure dans la programmation en 2020 aux États-Unis, pour ensuite me tourner vers un spécialisation en développement Front. C'est ainsi que j'ai obtenu une licence (RNCP Niveau 6) en développement web Javascript React.",
+    "Je continue de contribuer activement au sein de mon entreprise parisienne tout en cherchant où l'aventure me conduira après la fin de mon contrat. N'hésitez pas à me contacter via le formulaire ci-dessous ou votre réseau social préféré.",
     'Merci de votre visite!',
   ],
 }
