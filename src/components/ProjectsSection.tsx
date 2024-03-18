c7066cedeeb2f0c0923696e6a6c6754020bf8488
MarieRodiet <marie.rodiet@gmail.com>
2024-03-18 11:50:59 +0100

    fix padding of projects section

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -26,73 +26,77 @@ export default function ProjectsSection({ isEnglish }: Props) {
           {isEnglish ? 'Projects' : 'Projets'}
         </h1>
       </div>
-      <select
-        className="projectsSection-selectTypes"
-        value={selectedType}
-        onChange={(e) => {
-          const selected = (e.target as HTMLSelectElement).value as types
-          setType(selected)
-        }}
-      >
-        {projects_types.map((t, i) => (
-          <option
-            key={`${`${i}`}`}
-            value={`${`${t}`}`}
-            onClick={() => setType(t)}
-          >
-            {t}
-          </option>
-        ))}
-      </select>
+
       {!toggled ? (
-        <div className="projectsSection-container">
-          {filteredProjects.map((p, i) => (
-            <div
-              key={`${`${i} home page ${p.name}`}`}
-              className="projectsSection-container-project"
-            >
-              <h2 className="projectsSection-container-project-name">
-                {p.name}
-              </h2>
-              <div className="projectsSection-container-project-content">
-                <div className="projectsSection-container-project-content-links">
-                  <img
-                    className="projectsSection-container-project-content-links-img"
-                    src={p.img}
-                    alt={`home page ${p.name}}`}
-                  />
-                  <div className="projectsSection-container-project-content-links-linksContainer">
-                    <a
-                      className="projectsSection-container-project-content-links-linksContainer-website"
-                      href={p.url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {isEnglish ? 'See on Github' : 'Voir sur Github'}
-                    </a>
-                    {p.website ? (
+        <div>
+          <select
+            className="projectsSection-selectTypes"
+            value={selectedType}
+            onChange={(e) => {
+              const selected = (e.target as HTMLSelectElement).value as types
+              setType(selected)
+            }}
+          >
+            {projects_types.map((t, i) => (
+              <option
+                key={`${`${i}`}`}
+                value={`${`${t}`}`}
+                onClick={() => setType(t)}
+              >
+                {t}
+              </option>
+            ))}
+          </select>
+
+          <div className="projectsSection-container">
+            {filteredProjects.map((p, i) => (
+              <div
+                key={`${`${i} home page ${p.name}`}`}
+                className="projectsSection-container-project"
+              >
+                <h2 className="projectsSection-container-project-name">
+                  {p.name}
+                </h2>
+                <div className="projectsSection-container-project-content">
+                  <div className="projectsSection-container-project-content-links">
+                    <img
+                      className="projectsSection-container-project-content-links-img"
+                      src={p.img}
+                      alt={`home page ${p.name}}`}
+                    />
+                    <div className="projectsSection-container-project-content-links-linksContainer">
                       <a
                         className="projectsSection-container-project-content-links-linksContainer-website"
-                        href={p.website}
+                        href={p.url}
                         target="_blank"
-                        rel="noreferrer"
+                        rel="noopener noreferrer"
                       >
-                        {isEnglish ? 'See it live!' : 'Voir en live!'}
+                        {isEnglish ? 'See on Github' : 'Voir sur Github'}
                       </a>
-                    ) : null}
+                      {p.website ? (
+                        <a
+                          className="projectsSection-container-project-content-links-linksContainer-website"
+                          href={p.website}
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          {isEnglish ? 'See it live!' : 'Voir en live!'}
+                        </a>
+                      ) : null}
+                    </div>
+                  </div>
+                  <div className="projectsSection-container-project-content-description">
+                    {isEnglish ? p.en : p.fr}
+                  </div>
+                  <div className="projectsSection-container-project-content-tags">
+                    {p.tags.map((element, j) => (
+                      <span key={`${j + element}`}>{element}</span>
+                    ))}
                   </div>
                 </div>
-                <div className="projectsSection-container-project-content-description">
-                  {isEnglish ? p.en : p.fr}
-                </div>
-                <div className="projectsSection-container-project-content-tags">
-                  {p.tags.map((element, j) => (
-                    <span key={`${j + element}`}>{element}</span>
-                  ))}
-                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       ) : null}
     </section>
